f3665aafcffa85c4222b6c21fcc99f0900973152
BKurnia <82164413+BKurnia@users.noreply.github.com>
2022-05-20 17:45:39 +0700

    ketiga

--- lib/Fasilitas.dart
+++ lib/Fasilitas.dart
@@ -1,43 +1,81 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class Fasilitas extends StatelessWidget {
   const Fasilitas({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
- 
-                    ClipOval(
+      child: ListView(padding: EdgeInsets.all(20), shrinkWrap: true, children: [
+        InkWell(
+          child: Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: EdgeInsets.all(14),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: 350,
+                  child: Column(children: const[
+                    Text(
+                      "Gymnas",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                    ),
+                    Text(
+                      "Fasilitas olahraga bersama"
+                    )
+                  ],),
+                ),
+                Column(children: [
+                  ClipOval(
                     child: Image.network("https://cdn.pixabay.com/photo/2019/11/10/17/36/indonesia-4616370_1280.jpg",
                     width: 150,
                     height: 150,
                     fit: BoxFit.cover,),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                     child: Column(children: const[
+                    
+                  )
+                ],)
+              ],
+            ),
+          ),
+          onTap: () {
+            //gunakan navigator untuk panggil RincianFakultas
+            // Navigator.of(context).push(MaterialPageRoute(builder: (context){
+            //   return RincianFakultas();
+            // }));
+          },
+        ),
+        Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: EdgeInsets.all(14),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: 350,
+                  child: Column(children: const[
                     Text(
-                      "FPMIPA",
+                      "Stadium",
                       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
                     ),
                     Text(
-                      "Fakultas Pendidikan Matematika dan Ilmu Pengetahuan Alam"
+                      "Track lari dan stadiun sepak bola"
                     )
                   ],),
+                ),
+                Column(children: [
+                  ClipOval(
+                    child: Image.network("https://cdn.pixabay.com/photo/2019/11/10/17/36/indonesia-4616370_1280.jpg",
+                    width: 150,
+                    height: 150,
+                    fit: BoxFit.cover,),
+                    
                   )
-                ],
-              ),
-            )
-          ],
-        )
-      ),
+                ],)
+              ],
+            ),
+            ),
+      ]),
     );
   }
 }
--- lib/RincianFakultas.dart
+++ lib/RincianFakultas.dart
@@ -9,106 +9,37 @@ class RincianFakultas extends StatelessWidget {
         title: const Text('Rincian Fakultas'),
       ),
       body: Center(
-        child: ListView(padding: EdgeInsets.all(20), shrinkWrap: true, children: [
-        InkWell(
-          child: Container(
-            decoration: BoxDecoration(border: Border.all()),
-            padding: EdgeInsets.all(14),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(
-                  width: 350,
-                  child: Column(children: const[
-                    Text(
-                      "Gedung A",
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                    ),
-                    Text(
-                      "Gedung A FPMIPA umumnya digunakan untuk kuliah umum dan praktikum ipa"
-                    )
-                  ],),
-                ),
-                Column(children: [
-                  ClipOval(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+ 
+                    ClipOval(
                     child: Image.network("https://cdn.pixabay.com/photo/2019/11/10/17/36/indonesia-4616370_1280.jpg",
                     width: 150,
                     height: 150,
                     fit: BoxFit.cover,),
-                    
-                  )
-                ],)
-              ],
-            ),
-          ),
-          onTap: () {
-            //gunakan navigator untuk panggil RincianFakultas
-            Navigator.of(context).push(MaterialPageRoute(builder: (context){
-              return RincianFakultas();
-            }));
-          },
-        ),
-        Container(
-            decoration: BoxDecoration(border: Border.all()),
-            padding: EdgeInsets.all(14),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(
-                  width: 350,
-                  child: Column(children: const[
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                     child: Column(children: const[
                     Text(
-                      "Gedung B",
+                      "FPMIPA",
                       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
                     ),
                     Text(
-                      "Gedung A FPMIPA umumnya digunakan untuk kuliah umum"
+                      "Fakultas Pendidikan Matematika dan Ilmu Pengetahuan Alam adalah...."
                     )
                   ],),
-                ),
-                Column(children: [
-                  ClipOval(
-                    child: Image.network("https://cdn.pixabay.com/photo/2019/11/10/17/36/indonesia-4616370_1280.jpg",
-                    width: 150,
-                    height: 150,
-                    fit: BoxFit.cover,),
-                    
                   )
-                ],)
-              ],
-            ),
-            ),
-            Container(
-            decoration: BoxDecoration(border: Border.all()),
-            padding: EdgeInsets.all(14),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(
-                  width: 350,
-                  child: Column(children: const[
-                    Text(
-                      "Gedung C",
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                    ),
-                    Text(
-                      "Gedung A FPMIPA umumnya digunakan untuk kuliah dan praktikum ilmu komputer"
-                    )
-                  ],),
-                ),
-                Column(children: [
-                  ClipOval(
-                    child: Image.network("https://cdn.pixabay.com/photo/2019/11/10/17/36/indonesia-4616370_1280.jpg",
-                    width: 150,
-                    height: 150,
-                    fit: BoxFit.cover,),
-                    
-                  )
-                ],)
-              ],
-            ),
-            ),
-      ]),
+                ],
+              ),
+            )
+          ],
+        )
       ),
     );
   }
